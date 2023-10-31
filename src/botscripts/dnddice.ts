d28f835af6841785c7475f1ad19ff7d960b8cfef
BX <btbxbob@gmail.com>
2023-10-31 22:05:02 +0800

    doing dnd

--- src/botscripts/dnddice.ts
+++ src/botscripts/dnddice.ts
@@ -1,6 +1,6 @@
 class D20Dice {
-    d20(): number {
-        return Math.floor(Math.random() * 20) + 1;
+    roll(n:number): number {
+        return Math.floor(Math.random() * n) + 1;
     }
     proccess(reader: Function, sender: Function) {
     }
